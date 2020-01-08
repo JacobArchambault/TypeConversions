ef4ffeaebd9b0403ee5c3146f9684ece58e2b4d0
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-08 13:29:15 -0500

    Cast int as byte

--- Program.cs
+++ Program.cs
@@ -4,15 +4,25 @@ namespace TypeConversions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("***** Fun with type conversions *****");
 
             // Add two shorts and print the result
-            short numb1 = 9, numb2 = 10;
-            Console.WriteLine("{0} + {1} = {2}", numb1, numb2, Add(numb1, numb2));
+            short numb1 = 30000, numb2 = 30000;
+            short answer = (short)Add(numb1, numb2);
+            Console.WriteLine("{0} + {1} = {2}", numb1, numb2, answer);
+            NarrowingAttempt();
             Console.ReadLine();
         }
         static int Add(int x, int y) => x + y;
+        static void NarrowingAttempt()
+        {
+            int myInt = 200;
+
+            // Explicitly cast the int into a byte (no loss of data).
+            byte myByte = (byte)myInt;
+            Console.WriteLine("Value of myByte: {0}", myByte);
+        }
     }
 }
